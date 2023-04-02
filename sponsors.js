aa5f40fe1f36decc05dba054ef9d126324539d17
Sirred <harshitvarma103@gmail.com>
2023-04-02 16:43:21 +0530

    just the navbar, still faulty

---
+++ sponsors.js
@@ -0,0 +1,21 @@
+const navslide = () => {
+  const menu = document.querySelector(".menu");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  menu.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`;
+      }
+    });
+
+    menu.classList.toggle("toggle");
+  });
+};
+
+navslide();
